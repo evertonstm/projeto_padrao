502594d42a8f4c9e6c20dbeb3314858fa71dfa32
evertonstm <evertonstm@gmail.com>
2019-12-11 16:06:37 -0400

    Cadastro de Usuarios,  no banco, return basic

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,8 +2,20 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const user = await User.create(req)
-    return res.json();
+    const userExists = await User.findOne({ where: {email: req.body.email}});
+
+    if (userExists){
+      return res.status(400).json({ error: 'Usuário já existe.' });
+    }
+
+    const {id, name, email, provider } = await User.create(req.body)
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
